a21be5c4ef267485deb63b10dc649801610290d2
hallfjonas <hall.f.jonas@gmail.com>
2024-04-07 15:33:09 -0400

    fix: test

--- test/test_random_boundary_point.py
+++ test/test_random_boundary_point.py
@@ -9,15 +9,15 @@ from hytoperm import *
 class TestRandomBoundaryPoint(unittest.TestCase):
     
     def testRandomBoundaryPoint(self):
-        n_sets = 10
         ex = Experiment()
         assert(isinstance(ex, Experiment))
         ex.addRandomVoronoiPoints(10)
-        ex.addRandomTargets()
         ex.generatePartitioning()
-        ex.plotWorld()
-        p = ex._world.target(0).region().randomBoundaryPoint()
-        plt.plot(p[0][0], p[0][1], 'gd')
+        ex.addRandomTargets()
+        fig, ax = ex.plotWorld()
+        region = ex._world.target(0).region()
+        p = region.randomBoundaryPoint()
+        Node(p, set([region])).plot(ax, color='yellow', marker='d')
 
 
 if __name__ == "__main__":
