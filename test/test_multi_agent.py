8b5aa1209fb1b66b57516cf7c8a297b7a5f48158
hallfjonas <hall.f.jonas@gmail.com>
2024-04-12 15:20:57 -0400

    feat: imrove multi-agent test

--- test/test_multi_agent.py
+++ test/test_multi_agent.py
@@ -1,6 +1,7 @@
 
 # external imports
 import unittest
+import random
 
 # internal imports
 from hytoperm import *
@@ -12,10 +13,20 @@ class TestMultiAgent(unittest.TestCase):
         n_sets = 8
         ex = Experiment.generate(n_sets=n_sets, n_agents=3)
         assert(isinstance(ex, Experiment))
-        ex.agent().computeVisitingSequence()
-        ex.agent().initializeCycle()
-        ex.agent()._cycle.simulate()
-
-
+        ex.agent(0).computeVisitingSequence()
+        ex.agent(1).setTargetVisitingSequence(
+            [ex.world().target(i) for i in range(ex.world().nTargets())]
+            )
+        ex.agent(2).setTargetVisitingSequence(
+            random.shuffle(
+                [ex.world().target(i) for i in range(ex.world().nTargets())]
+                )
+            )
+        
+        for agent in ex.agents():
+            agent.computeVisitingSequence()
+            agent.initializeCycle()
+            agent._cycle.simulate()
+            
 if __name__ == "__main__":
     unittest.main()
