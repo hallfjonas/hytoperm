4d858cd701c15df82bd3f6a52fd3a674433c9bfa
hallfjonas <hall.f.jonas@gmail.com>
2024-11-28 12:18:30 -0500

    feat: add palettes

---
+++ hytoperm/PyPlotHelpers/Palettes.py
@@ -0,0 +1,56 @@
+
+"""
+    Beautiful pastel color palettes. Source:
+        https://www.pinterest.com/pin/796855727798583802/
+"""
+
+pastel_mix = [
+    "#ABDEE6",
+    "#CBAACB",
+    "#FFFFB5",
+    "#FFCCB6",
+    "#F3B0C3"
+]
+
+pastel_soft = [
+    "#C6DBDA",
+    "#FEE1E8",
+    "#FED7C3",
+    "#F6EAC2",
+    "#ECD5E3"
+]
+
+pastel_warm = [
+    "#FF968A",
+    "#FFAEA5",
+    "#FFC5BF",
+    "#FFD8BE",
+    "#FFC8A2"
+]
+
+pastel_cold = [
+    "#D4F0F0",
+    "#8FCACA",
+    "#CCE2CB",
+    "#B6CFB6",
+    "#97C1A9"
+]
+
+pastel_warm_cold = [
+    "#FCB9AA",
+    "#FFDBCC",
+    "#ECEAE4",
+    "#A2E1DB",
+    "#55CBCD"
+]
+
+pastel_strong = [
+    pastel_mix[1],
+    pastel_cold[-1],
+    pastel_warm[0],
+    pastel_warm[-1],
+    pastel_mix[0],
+    pastel_mix[-1],
+    pastel_warm_cold[-1],
+    pastel_mix[2]
+]
